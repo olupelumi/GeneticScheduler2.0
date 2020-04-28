2939e2c6730561119b66a018ef5aeaf1735d5a80
Oluwapelumi <pelumifafowora@gmail.com>
2020-04-28 14:31:30 -0400

    made the content of the schedule a pointer

--- ScheduleAgent.py
+++ ScheduleAgent.py
@@ -1,10 +1,10 @@
 #class that represents a scedule
 import json
 class Schedule:
-    def __init__(self):
+    def __init__(self, json_info = None):
         self.fitness = None
         #The actual schedule information
-        self.content = None
+        self.content = [] #WIll be an array of sets
         
     def from_json(self, json_data):
         pass
