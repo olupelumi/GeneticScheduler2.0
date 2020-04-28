a5df41bae2588da55d592631efb673d35391ef16
Oluwapelumi <pelumifafowora@gmail.com>
2020-04-28 00:48:39 -0400

    scaffolding for schedule class

--- DNA.py
+++
@@ -1,1 +0,0 @@
-#DNA.java but in python
---
+++ ScheduleAgent.py
@@ -0,0 +1,16 @@
+#class that represents a scedule
+import json
+class Schedule:
+    def __init__(self):
+        self.fitness = None
+        #The actual schedule information
+        self.content = None
+        
+    def from_json(self, json_data):
+        pass
+
+    def __str__(self):
+        pass
+
+    def to_json(self):
+        pass
