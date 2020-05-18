118dfcba11a5fc12f2c90f9c794665c764683a62
Oluwapelumi <pelumifafowora@gmail.com>
2020-05-18 19:47:26 -0400

    added regular expressions to check if the text I'm looking at is a shift

--- Survey.py
+++ Survey.py
@@ -1,6 +1,6 @@
 #Survey.java but in python
 import json
-
+import re
 '''
  * Survey Data structure to represent the information coming from the inputted survey need the list
  * of names preference information as a mapping of a mapping(name to day/time to the number they
@@ -21,56 +21,56 @@ import json
 
 #shifts in a sorted manner
 shift_list = [
-'Monday (6/24) [7:30am-8:55am]',
-'Monday (6/24) [8:55am-9:55am]',
-'Monday (6/24) [9:55am-10:55am]',
-'Monday (6/24) [10:55am-11:55am]',
-'Monday (6/24) [11:55am-12:55pm]',
-'Monday (6/24) [12:55 pm-1:55pm]',
-'Monday (6/24) [1:55pm-2:55pm]',
-'Monday (6/24) [2:55pm-3:55pm]',
-'Monday (6/24) [3:55pm-4:55pm]',
-'Monday (6/24) [4:55pm-close]',
-'Tuesday (6/25) [7:30am-8:55am]',
-'Tuesday (6/25) [8:55am-9:55am]',
-'Tuesday (6/25) [9:55am-10:55am]',
-'Tuesday (6/25) [10:55am-11:55am]',
-'Tuesday (6/25) [11:55am-12:55pm]',
-'Tuesday (6/25) [12:55pm-1:55pm]',
-'Tuesday (6/25) [1:55pm-2:55pm]',
-'Tuesday (6/25) [2:55pm-3:55pm]',
-'Tuesday (6/25) [3:55pm- 4:55 pm]',
-'Tuesday (6/25) [4:55pm-close]',
-'Wednesday (6/26) [7:30am-8:55am]',
-'Wednesday (6/26) [8:55am-9:55am]',
-'Wednesday (6/26) [9:55am-10:55am]',
-'Wednesday (6/26) [10:55am-11:55am]',
-'Wednesday (6/26) [11:55am-12:55pm]',
-'Wednesday (6/26) [12:55pm-1:55pm]',
-'Wednesday (6/26) [1:55pm-2:55 pm]',
-'Wednesday (6/26) [2:55pm-3:55 pm]',
-'Wednesday (6/26) [3:55pm-4:55 pm]',
-'Wednesday (6/26) [4:55pm-close]',
-'Thursday (6/27) [7:30am-8:55am]',
-'Thursday (6/27) [8:55am-9:55am]',
-'Thursday (6/27) [9:55am-10:55am]',
-'Thursday (6/27) [10:55am-11:55am]',
-'Thursday (6/27) [11:55am-12:55pm]',
-'Thursday (6/27) [12:55pm-1:55pm]',
-'Thursday (6/27) [1:55pm-2:55pm]',
-'Thursday (6/27) [2:55pm-3:55pm]',
-'Thursday (6/27) [3:55pm-4:55pm]',
-'Thursday (6/27) [4:55pm-close]',
-'Friday (6/28) [7:30am-8:55am]',
-'Friday (6/28) [8:55am-9:55am]',
-'Friday (6/28) [9:55am-10:55am]',
-'Friday (6/28) [10:55am-11:55am]',
-'Friday (6/28) [11:55am-12:55pm]',
-'Friday (6/28) [12:55pm-1:55pm]',
-'Friday (6/28) [1:55pm-2:55pm]',
-'Friday (6/28) [2:55pm-3:55pm]',
-'Friday (6/28) [3:55pm-4:55pm]',
-'Friday (6/28) [4:55pm-close]'
+'Monday (6/24) [7:30 am - 8:55 am]',
+'Monday (6/24) [8:55 am - 9:55 am]',
+'Monday (6/24) [9:55 am - 10:55 am]',
+'Monday (6/24) [10:55 am - 11:55 am]',
+'Monday (6/24) [11:55 am - 12:55 pm]',
+'Monday (6/24) [12:55 pm - 1:55 pm]',
+'Monday (6/24) [1:55 pm - 2:55 pm]',
+'Monday (6/24) [2:55 pm - 3:55 pm]',
+'Monday (6/24) [3:55 pm - 4:55 pm]',
+'Monday (6/24) [4:55 pm - close]',
+'Tuesday (6/25) [7:30 am - 8:55 am]',
+'Tuesday (6/25) [8:55 am - 9:55 am]',
+'Tuesday (6/25) [9:55 am - 10:55 am]',
+'Tuesday (6/25) [10:55 am - 11:55 am]',
+'Tuesday (6/25) [11:55 am - 12:55 pm]',
+'Tuesday (6/25) [12:55 pm - 1:55 pm]',
+'Tuesday (6/25) [1:55 pm - 2:55 pm]',
+'Tuesday (6/25) [2:55 pm - 3:55 pm]',
+'Tuesday (6/25) [3:55 pm - 4:55 pm]',
+'Tuesday (6/25) [4:55 pm - close]',
+'Wednesday (6/26) [7:30 am - 8:55 am]',
+'Wednesday (6/26) [8:55 am - 9:55 am]',
+'Wednesday (6/26) [9:55 am - 10:55 am]',
+'Wednesday (6/26) [10:55 am - 11:55 am]',
+'Wednesday (6/26) [11:55 am - 12:55 pm]',
+'Wednesday (6/26) [12:55 pm - 1:55 pm]',
+'Wednesday (6/26) [1:55 pm - 2:55 pm]',
+'Wednesday (6/26) [2:55 pm - 3:55 pm]',
+'Wednesday (6/26) [3:55 pm - 4:55 pm]',
+'Wednesday (6/26) [4:55 pm - close]',
+'Thursday (6/27) [7:30 am - 8:55 am]',
+'Thursday (6/27) [8:55 am - 9:55 am]',
+'Thursday (6/27) [9:55 am - 10:55 am]',
+'Thursday (6/27) [10:55 am - 11:55 am]',
+'Thursday (6/27) [11:55 am - 12:55 pm]',
+'Thursday (6/27) [12:55 pm - 1:55 pm]',
+'Thursday (6/27) [1:55 pm - 2:55 pm]',
+'Thursday (6/27) [2:55 pm - 3:55 pm]',
+'Thursday (6/27) [3:55 pm - 4:55 pm]',
+'Thursday (6/27) [4:55 pm - close]',
+'Friday (6/28) [7:30 am - 8:55 am]',
+'Friday (6/28) [8:55 am - 9:55 am]',
+'Friday (6/28) [9:55 am - 10:55 am]',
+'Friday (6/28) [10:55 am - 11:55 am]',
+'Friday (6/28) [11:55 am - 12:55 pm]',
+'Friday (6/28) [12:55 pm - 1:55 pm]',
+'Friday (6/28) [1:55 pm - 2:55 pm]',
+'Friday (6/28) [2:55 pm - 3:55 pm]',
+'Friday (6/28) [3:55 pm - 4:55 pm]',
+'Friday (6/28) [4:55 pm - close]'
 ]
 
 #creating my mapping of shifts
@@ -101,13 +101,15 @@ def compute_score_matx(jdata):
         for shift in name_pref_dict.keys():
             #here I need to get the index of the shift
             #Need to error check now
-            score_matx[name_val][shift] = shift_to_idx_map[shift]
+            #Thinking I could use regular expressions since the strings representing the shifts are of the same pattern. 
+            if re.match(r"[a-zA-Z]+\s\(\d[\d]?/\d[\d]?\)\s\[\d[\d]?:\d[\d]?[\s]?[ap]m[\s]?-[\s]?(\d[\d]?:\d[\d]?[\s]?[ap]m|close)\]",shift):
+                #score_matx[name_val][shift] = shift_to_idx_map[shift]
         
-
 
 
 with open("survey_data.txt") as jsonfile:
     #json_data information
     json_data = json.load(jsonfile)
-#print(json_data)
+    #print(json_data)
 
+compute_score_matx(json_data)
