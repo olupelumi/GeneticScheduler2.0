ea87d1f17ce9dbab4d9a605abc98bda648d182a4
Oluwapelumi <pelumifafowora@gmail.com>
2020-05-20 00:46:54 -0400

    implemented the select agents method as well as initiating the population

--- Population.py
+++ Population.py
@@ -1,13 +1,35 @@
 #Population.java but in python
 import json
-
+import random
+import ScheduleAgent
 class Population:
-    def __init__(self, population_numb):
-        self.agent_list = []
+    def __init__(self, population_numb, numShifts):
         self.num_pop = population_numb
+        self.numShifts = numShifts
+        self.agent_list = [ScheduleAgent.Schedule(self.numShifts) for _ in range(self.num_pop)]
 
     def select_agents(self):
-        pass
+        """
+        Requires:
+        Nothing
+
+        Effect:
+        selects a mating pool of agents to use to mutate and crossbreed
+        returns an aray of selected agents
+        """
+          #Chooses the top two most fit agents
+
+        #sorting agents by their fitness
+        sorted_agents = sorted(self.agent_list, key = lambda agent: agent.fitness, reverse = True)
+        
+        rand1 = random.randint(0, 10)
+        rand2 = random.randint(0, 10)
+    
+        parent1 = sorted_agents[rand1]
+        parent2 = sorted_agents[rand2]
+
+        print("parent 1: {},\n parent 2: {}\n".format(parent1, parent2))
+        return (parent1, parent2)
 
     def crossbreed(self):
         pass
